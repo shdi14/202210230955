7cf8ca37ad4f1201abcdb3f7a37db5fd111bb801
shdi14 <116509953+shdi14@users.noreply.github.com>
2022-10-23 20:00:34 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,157 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
+scene.onHitWall(SpriteKind.Player, function (sprite, location) {
+    tiles.placeOnRandomTile(mySprite, sprites.castle.tilePath5)
+    info.changeScoreBy(12)
+})
+function flyingBat () {
+    mySprite2 = sprites.create(img`
+        . f f f . . . . . . . . f f f . 
+        f f c . . . . . . . f c b b c . 
+        f c c . . . . . . f c b b c . . 
+        c f . . . . . . . f b c c c . . 
+        c f f . . . . . f f b b c c . . 
+        f f f c c . c c f b c b b c . . 
+        f f f c c c c c f b c c b c . . 
+        . f c 3 c c 3 b c b c c c . . . 
+        . c b 3 b c 3 b b c c c c . . . 
+        c c b b b b b b b b c c . . . . 
+        c 1 1 b b b 1 1 b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        f b c b b b c b b b b f . . . . 
+        . f 1 f f f 1 b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `, SpriteKind.Enemy)
+    x_bat = 50
+    y_bat = 50
+    animation.runImageAnimation(
+    mySprite2,
+    [img`
+        . . f f f . . . . . . . . f f f 
+        . f f c c . . . . . . f c b b c 
+        f f c c . . . . . . f c b b c . 
+        f c f c . . . . . . f b c c c . 
+        f f f c c . c c . f c b b c c . 
+        f f c 3 c c 3 c c f b c b b c . 
+        f f b 3 b c 3 b c f b c c b c . 
+        . c b b b b b b c b b c c c . . 
+        . c 1 b b b 1 b b c c c c . . . 
+        c b b b b b b b b b c c . . . . 
+        c b c b b b c b b b b f . . . . 
+        f b 1 f f f 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `,img`
+        . . f f f . . . . . . . . . . . 
+        f f f c c . . . . . . . . f f f 
+        f f c c . . c c . . . f c b b c 
+        f f c 3 c c 3 c c f f b b b c . 
+        f f b 3 b c 3 b c f b b c c c . 
+        . c b b b b b b c f b c b c c . 
+        . c b b b b b b c b b c b b c . 
+        c b 1 b b b 1 b b b c c c b c . 
+        c b b b b b b b b c c c c c . . 
+        f b c b b b c b b b b f c . . . 
+        f b 1 f f f 1 b b b b f c c . . 
+        . f b b b b b b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . c c . . c c . . . . . . . . 
+        . . c 3 c c 3 c c c . . . . . . 
+        . c b 3 b c 3 b c c c . . . . . 
+        . c b b b b b b b b f f . . . . 
+        c c b b b b b b b b f f . . . . 
+        c b 1 b b b 1 b b c f f f . . . 
+        c b b b b b b b b f f f f . . . 
+        f b c b b b c b c c b b b . . . 
+        f b 1 f f f 1 b f c c c c . . . 
+        . f b b b b b b f b b c c . . . 
+        c c f b b b b b c c b b c . . . 
+        c c c f f f f f f c c b b c . . 
+        . c c c . . . . . . c c c c c . 
+        . . c c c . . . . . . . c c c c 
+        . . . . . . . . . . . . . . . . 
+        `,img`
+        . f f f . . . . . . . . f f f . 
+        f f c . . . . . . . f c b b c . 
+        f c c . . . . . . f c b b c . . 
+        c f . . . . . . . f b c c c . . 
+        c f f . . . . . f f b b c c . . 
+        f f f c c . c c f b c b b c . . 
+        f f f c c c c c f b c c b c . . 
+        . f c 3 c c 3 b c b c c c . . . 
+        . c b 3 b c 3 b b c c c c . . . 
+        c c b b b b b b b b c c . . . . 
+        c b 1 b b b 1 b b b b f c . . . 
+        f b b b b b b b b b b f c c . . 
+        f b c b b b c b b b b f . . . . 
+        . f 1 f f f 1 b b b c f . . . . 
+        . . f b b b b b b c f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        `],
+    200,
+    true
+    )
+    while (true) {
+        move(mySprite2, x_bat, y_bat, randint(0, 100), randint(0, 150), 1000, 30)
+    }
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    info.changeLifeBy(-1)
+})
+function move (sprite: Sprite, x1: number, y1: number, x2: number, y2: number, time: number, frame: number) {
+    framesCount = time / 1000 * frame
+    framePause = time / framesCount
+    for (let index = 0; index <= framesCount; index++) {
+        dx = (x2 - x1) / framesCount
+        dy = (y2 - y1) / framesCount
+        mySprite2.setPosition(x1 + dx * index, y1 + dy * index)
+        pause(framePause)
+    }
+    x_bat = x2
+    y_bat = y2
+}
+sprites.onCreated(SpriteKind.Player, function (sprite) {
+	
+})
+let dy = 0
+let dx = 0
+let framePause = 0
+let framesCount = 0
+let y_bat = 0
+let x_bat = 0
+let mySprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . . . f f f f f . . . . . . . 
+    . . . f e e e e e f . . . . . . 
+    . . f d d d d e e e f f . . . . 
+    . c d d d d d d e e d d f . . . 
+    . c d f d d f d e e b d c . . . 
+    c d d f d d f d e e b d c . f f 
+    c d e e d d d d e e f c . f e f 
+    c d d d d c d e e e f . . f e f 
+    . f c c c d e e e f f . . f e f 
+    . . f f f f f e e e e f . f e f 
+    . . . . f e e e e e e e f f f . 
+    . . f f e f e e f e e e e f . . 
+    . f e f f e e f f f e e e f . . 
+    f d d b d d c f f f f f f b f . 
+    f d d c d d d f . . f c d d f . 
+    . f f f f f f f . . . f f f . . 
+    `, SpriteKind.Player)
+scene.cameraFollowSprite(mySprite)
+controller.moveSprite(mySprite)
+tiles.setCurrentTilemap(tilemap`level2`)
+info.setLife(5)
+flyingBat()
